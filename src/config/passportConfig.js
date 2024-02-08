ccc690dff533b734095feea6e8cb4b2673e9415e
AnderMichael <andercaymi@gmail.com>
2024-02-07 23:15:27 -0400

    fix: Completing .env.example file

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -2,64 +2,64 @@
 const passport = require("passport");
 const session = require("express-session");
 const LocalStrategy = require("passport-local").Strategy;
+const { env: envConfig, passport: passportConfig } = require("./config");
 const User = require("../models/user.model");
-const env = process.env.NODE_ENV || "development";
-require("dotenv").config();
+const env = envConfig.mode;
 
 // Function to configure authentication session options
 const configureSessionOptions = () => {
-    const sessionOptions = {
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 30, // Cookie expiry time = 1 month (in milliseconds)
+  const sessionOptions = {
+    secret: passportConfig.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30, // Cookie expiry time = 1 month (in milliseconds)
+    },
+  };
+
+  if (env == "production") {
+    sessionOptions.cookie.sameSite = "none";
+    sessionOptions.cookie.secure = true;
+    sessionOptions.cookie.domain = "netlify.app";
+  }
+
+  return sessionOptions;
+};
+
+// Function to configure Passport
+const configurePassport = (app) => {
+  // Configure authentication session options
+  const sessionOptions = configureSessionOptions();
+  app.use(session(sessionOptions));
+
+  // Configure LocalStrategy
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "username",
+        passwordField: "password",
       },
+      User.authenticate()
+    )
+  );
+
+  // Initialize Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Serialize and deserialize user
+  passport.serializeUser((user, done) => {
+    const sessionUser = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      userType: user.userType,
     };
-  
-    if (env == "production") {
-      sessionOptions.cookie.sameSite = "none";
-      sessionOptions.cookie.secure = true;
-      sessionOptions.cookie.domain = "netlify.app";
-    }
-  
-    return sessionOptions;
-  };
-  
-  // Function to configure Passport
-  const configurePassport = (app) => {
-    // Configure authentication session options
-    const sessionOptions = configureSessionOptions();
-    app.use(session(sessionOptions));
-  
-    // Configure LocalStrategy
-    passport.use(
-      new LocalStrategy(
-        {
-          usernameField: "username",
-          passwordField: "password",
-        },
-        User.authenticate()
-      )
-    );
-  
-    // Initialize Passport
-    app.use(passport.initialize());
-    app.use(passport.session());
-  
-    // Serialize and deserialize user
-    passport.serializeUser((user, done) => {
-      const sessionUser = {
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-        userType: user.userType,
-      };
-      done(null, sessionUser);
-    });
-    passport.deserializeUser((sessionUser, done) => {
-      done(null, sessionUser);
-    });
-  };
-  
-  module.exports =  configurePassport;
+    done(null, sessionUser);
+  });
+  passport.deserializeUser((sessionUser, done) => {
+    done(null, sessionUser);
+  });
+};
+
+module.exports = configurePassport;
