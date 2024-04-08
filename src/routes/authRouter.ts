b4e226c105e66ed586a3c47dd8790422b9a91aa7
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2024-04-08 10:13:08 -0400

    feat#20: moved auth routes from userRoutes to authRoutes

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,24 @@
 import express from 'express';
 import { authController } from '../controllers'; // Adjust the path as needed
 import { validateRefreshToken } from '../middlewares'; // Adjust the path as needed
+import { userController } from '../controllers';
+import {
+    validateRegisterInput,
+    validateLoginInput,
+    validateForgotPasswordInput,
+    verifyPermissions,
+    userAuth,
+  } from '../middlewares';
 
 const authRouter = express.Router();
 
+
+authRouter.post('/login', validateLoginInput, userController.login);
+authRouter.get('/logout', userController.logout);
+authRouter.get('/isLoggedIn', userAuth, userController.checkAuthentication);
+authRouter.post('/forgot-password', validateForgotPasswordInput, userController.forgotPassword);
+authRouter.post('/reset-password/:token', userController.resetPassword);
+authRouter.get('/verify/:token', userController.verifyEmail);
 authRouter.post('/refresh-token', validateRefreshToken, authController.refreshToken);
 
 export default authRouter;
