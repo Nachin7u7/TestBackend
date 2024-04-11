7d72740262f7c69d1bbff3861a8fe0badd3bd153
Zein Tonconi <zeintonconi@gmail.com>
2024-04-11 18:57:44 -0400

    feat#22: Controller and Service OOP

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import {userRouter, authRouter, problemRouter} from '../routes';
+import { problemRouter} from '../routes';
+import { AuthController } from '../controllers/authController';
+import userRouter from '../routes/userRouter';
+import { AuthService } from '../services/authService';
+import { UserRepositoryImpl } from '../repositories/implements/userRepositoryImpl';
 
 const router = express.Router();
 
+const userRepository = new UserRepositoryImpl()
+const authService = new AuthService(userRepository)
+const authController = new AuthController(authService)
+
 router.use('/users', userRouter);
 router.use('/problem', problemRouter);
-router.use('/auth', authRouter);
+router.use('/auth', authController.router)
 
 export default router;
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,14 @@
-import { UserRepository } from './../repositories/userRepository';
+
 import { services } from '../services';
 import { HTTP_STATUS } from '../constants';
 import { buildLogger } from '../plugin';
 import { AuthService } from '../services/authService';
-import * as successHandler from '../handlers/successHandler'; // Adjust based on your actual file structure
 import { UserRepositoryImpl } from '../repositories/implements/userRepositoryImpl';
 
 const {
   getUsersSortedBySolvedProblems,
   registerUser,
-  registerAdminUser,
-  checkTokenToMail,
+  registerAdminUser
 } = services;
 const userRepository = new UserRepositoryImpl(); 
 const authService = new AuthService(userRepository);
@@ -66,102 +64,8 @@ const registerAdmin = async (req: any, res: any): Promise<any> => {
   }
 };
 
-const verifyEmail = async (req: any, res: any): Promise<any> => {
-  try {
-    const { token } = req.params;
-    logger.log('Verifying email with token:', token);
-    const verifcationResult = await checkTokenToMail(token);
-    logger.log('Email verified successfully');
-    res
-      .status(HTTP_STATUS.OK)
-      .json({ message: 'Email verified successfully. You can now login.' });
-  } catch (error: any) {
-    logger.error('Error verifying email:', error);
-    res
-      .status(HTTP_STATUS.BAD_REQUEST)
-      .json({ message: 'Failed to verify email. Invalid or expired token.' });
-  }
-};
-
-const login = async (req: any, res: any): Promise<any> => {
-  try {
-    const { username, password } = req.body;
-    logger.log('Attempting to log in user:', { user: username });
-    const loginResponseData = await authService.authenticateUser(username, password);
-
-    successHandler.sendOkResponse(
-      res,
-      loginResponseData,
-      'Logged in successfully'
-    );
-  } catch (error: any) {
-    logger.error('Login error', { error: error.message });
-    res.status(HTTP_STATUS.UNAUTHORIZED).json({ message: error.message });
-  }
-};
-
-const logout = (req: any, res: any) => {
-  logger.log('User logged out successfully');
-  res.status(HTTP_STATUS.OK).json({ message: 'Successfully logged out.' });
-};
-
-const checkAuthentication = (req: any, res: any) => {
-  const { user } = req;
-  if (user) {
-    logger.log('User is authenticated:', { user: req.user });
-    res.status(HTTP_STATUS.OK).json({
-      isAuthenticated: true,
-      user: user,
-    });
-  } else {
-    logger.log('User is not authenticated');
-    res.status(HTTP_STATUS.OK).json({ isAuthenticated: false });
-  }
-};
-
-const forgotPassword = async (req: any, res: any) => {
-  try {
-    const { email } = req.body;
-    logger.log('Forgot password request for email:', email);
-    await authService.sendForgotPasswordEmail(email);
-    logger.log('Forgot password email sent successfully');
-    successHandler.sendOkResponse(
-      res,
-      null,
-      'Forgot password email sent successfully'
-    );
-  } catch (error: any) {
-    logger.error('Error sending forgot password email:', error);
-    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-      message: 'Failed to send email. Please try again.',
-    });
-  }
-};
-
-const resetPassword = async (req: any, res: any) => {
-  try {
-    const { token } = req.params;
-    const { password } = req.body;
-    logger.log('Resetting password for token:', token);
-    await authService.resetUserPassword(token, password);
-    logger.log('Password reset successfully');
-    successHandler.sendOkResponse(res, null, 'Password reset successfully');
-  } catch (error: any) {
-    logger.error('Error resetting password:', error);
-    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-      message: 'Failed to reset password. Please try again.',
-    });
-  }
-};
-
 export {
   globalLeaderboard,
   register,
   registerAdmin,
-  verifyEmail,
-  login,
-  logout,
-  checkAuthentication,
-  forgotPassword,
-  resetPassword,
 };
